da1901795693233d9e1f24143c2674a23a8d32ef
hjt <1092629609@qq.com>
2019-11-05 19:59:24 +0800

    提交5

--- src/main/java/com/sc/crm/bean/test.java
+++
@@ -1,6 +0,0 @@
-package com.sc.crm.bean;
-
-public class test {
-
-	private Integer id;
-}
--- src/main/java/com/sc/crm/bean/test2.java
+++ src/main/java/com/sc/crm/bean/test2.java
@@ -3,4 +3,5 @@ package com.sc.crm.bean;
 public class test2 {
 
 	private Integer id;
+	private String name;
 }
